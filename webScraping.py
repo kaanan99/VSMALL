7534ca31ec09b9877c80955c5af61ee316f2f524
Kaanan Kharwa <kaanan99@gmail.com>
2021-02-14 20:15:43 -0800

    new way of getting image

--- webScraping.py
+++ webScraping.py
@@ -1,6 +1,7 @@
 from lxml import html
 import requests
 import re
+from flask import jsonify
 class Cloth:
    def __init__(self, brand, name, price, image, sale, link):
       self.name = name
@@ -12,6 +13,7 @@ class Cloth:
       self.link = link
 
 def scrapeHollister(link, admin):
+   print("Function called")
    clothes = []
    admin_user_agents = {"Kaanan": {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:85.0) Gecko/20100101 Firefox/85.0'}}
    #header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:85.0) Gecko/20100101 Firefox/85.0'}
@@ -19,10 +21,11 @@ def scrapeHollister(link, admin):
    tree = html.fromstring(pageContent.content)
    name = tree.xpath('/html/body/div[5]/div[5]/main/div/ul/li/div/div[3]/div[1]/a/text()')
    price = tree.xpath('/html/body/div[5]/div[5]/main/div/ul/li/div/div[3]/div[3]/div[1]/div/span/span[2]/span[@data-state = "" or @data-state = "discount"]/text()')
-   image = tree.xpath('/html/body/div[5]/div[5]/main/div/ul/li/div/div[2]/div[1]/a/img/@src')
+   image = tree.xpath('/html/body/div[5]/div[5]/main/div/ul/li/div/div[2]/div[1]/a/img/@data-src')
    sale = tree.xpath('/html/body/div[5]/div[5]/main/div/ul/li/div/div[3]/div[3]/div[1]/div/span/span[2]/span[@data-state = "" or @data-state = "discount"]/@data-state')
    link = tree.xpath('/html/body/div[5]/div[5]/main/div/ul/li/div/div[2]/div[1]/a/@href')
    for x in range(len(name)):
-      clothes.append(Cloth("Hollister" , name[x], price[x].strip(), image[x], sale[x], "https://www.hollisterco.com" + link[x]))
+      new_image = image[x].replace("imageType", "prod1")
+      clothes.append(Cloth("Hollister" , name[x], price[x].strip(), new_image, sale[x], "https://www.hollisterco.com" + link[x]))
    return clothes
 
