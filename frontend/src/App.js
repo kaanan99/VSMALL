c5a30ff1b38ba04bdb226d167346c4109a8aeeb8
Parth Ray <parthray16@gmail.com>
2021-03-02 23:40:48 -0800

    added two filters

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -91,28 +91,12 @@ class CatalogPage extends Component  {
     }
   }
 
-  state = {
-    items: []
-  }
-  
-  componentDidMount () {
-    axios.get('http://localhost:5000/catalog')
-      .then(res => {
-        const items = res.data.items_list
-        this.setState({ items })
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-  }
-
   render () {
-    const { items } = this.state
     return (
       <div className="container">
         <h2 align='center'>Welcome to VSMall</h2>
         <h2 align='center'>{this.props.whichtodisplay()}</h2>
-        <Catalog items_list={items}/>
+        <Catalog />
       </div>
     )    
   }
--- frontend/src/Catalog.js
+++ frontend/src/Catalog.js
@@ -3,20 +3,53 @@ import ItemCard from './ItemCard'
 import { Row, Col } from 'react-simple-flex-grid'
 import 'react-simple-flex-grid/lib/main.css'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 
 class Catalog extends Component {
-  static get propTypes () {
-    return {
-      items_list: PropTypes.any
-    }
+  state = {items_list: []}
+
+  componentDidMount () {
+    axios.get('http://localhost:5000/catalog')
+      .then(res => {
+        const items = res.data.items_list
+        this.setState({ items_list : items})
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+
+  showAll () {
+    axios.get('http://localhost:5000/catalog')
+      .then(res => {
+        const items = res.data.items_list
+        this.setState({ items_list: items })
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+
+  filterBySale () {
+    axios.get('http://localhost:5000/catalog?sale=Sale')
+      .then(res => {
+        const items = res.data.items_list
+        this.setState({ items_list : items })
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
   }
 
   render () {
+    const {items_list} = this.state
     return (
         <div className="Catalog">
+          <button name="ShowAll" onClick={() => this.showAll()}>Show All</button>
+          <button name="FilterbySale" onClick={() => this.filterBySale()}>Sale</button>
           <Row gutter={40}>
             {
-            (this.props.items_list).map(item =>
+            items_list.map(item =>
               <Col
                 key = {item.name}
                 xs={{ span: 6 }} sm={{ span: 4 }}
