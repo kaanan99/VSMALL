1f1ee068b7971a3fe6a475bd05e312d3ccc5dd64
Kaanan Kharwa <kaanan99@gmail.com>
2021-03-03 00:16:38 -0800

    Adding WishlistButton.js

---
+++ frontend/src/WishListButton.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
+class WishListButton extends Component {
+  static get propTypes () {
+    return {
+      isSignedIn: PropTypes.any,
+      user: PropTypes.any
+    }
+  }
+
+  handleClick () {
+    if (this.props.isSignedIn) {
+      alert('before')
+      alert(this.props.user)
+      alert('after')
+    } else {
+      alert('before')
+      alert('You need to log in to add items to your wishlist')
+    }
+  }
+
+  render () {
+    return (<button name="WishList" onClick = {() => this.handleClick()}>Add to WishList</button>)
+  }
+}
+export default WishListButton
