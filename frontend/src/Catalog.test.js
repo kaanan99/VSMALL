155511d5b7982d800cfb73f0b65ead876729cb82
Kaanan Kharwa <kaanan99@gmail.com>
2021-03-08 11:27:22 -0800

    Did catalog and wishlist

--- frontend/src/Catalog.test.js
+++ frontend/src/Catalog.test.js
@@ -9,3 +9,13 @@ test('should render an empty Catalog', () => {
     expect(item).toStrictEqual([]);
 });
 
+// test('should render something', () => {
+//     const itemCard = render(<ItemCard image = {"image"} name = {"name"} price = {null} link = {null} sale = {null} brand = {"brand"}/>);
+//     const x = [itemCard];
+//     const catalog = render(<Catalog items_list = {x}/>);
+//     const item = catalog.getByRole('ItemCard');
+//     const name = item.getAttribute('name');
+//     expect(name).toBe(null)
+    
+// });
+
--- frontend/src/ItemCard.test.js
+++ frontend/src/ItemCard.test.js
@@ -3,35 +3,25 @@ import { render, fireEvent } from '@testing-library/react';
 
 test('should render empty ItemCard', () => {
    const itemCard = render(<ItemCard image = {null} name = {null} price = {null} link = {null} sale = {null} />);
-<<<<<<< HEAD
-   const header = itemCard.getAllByRole('heading')[1];
-=======
    const headers = itemCard.getAllByRole('heading');
->>>>>>> 6a83869e9112bc855f10c0816ce7bd7187411c4c
    const image = itemCard.getByRole('img');
    expect(headers[0].innerHTML).toBe('');
    expect(image.getAttribute('src')).toBe(null);
 });
 
 test('Has stuff in it', () => {
-<<<<<<< HEAD
-   const itemCard = render(<ItemCard image = {"image"} name = {"name"} price = {null} link = {null} sale = {null} />);
-   const header = itemCard.getAllByRole('heading')[1];
-=======
    const itemCard = render(<ItemCard image = {"image"} name = {"name"} price = {null} link = {null} sale = {null} brand = {"brand"}/>);
    const headers = itemCard.getAllByRole('heading');
->>>>>>> 6a83869e9112bc855f10c0816ce7bd7187411c4c
    const image = itemCard.getByRole('img');
    expect(headers[1].innerHTML).toBe('name');
    expect(headers[0].innerHTML).toBe('brand');
    expect(image.getAttribute('src')).toBe('image');
 });
-/*
-test('Right onClick is called', () => {
-   const onClick = jest.fn();
-   const itemCard = render(<ItemCard image = {null} name = {null} price = {null} link = {null} sale = {null} onClick = {onClick}/>);
-   const button  = itemCard.getByRole('ItemCard');
-   fireEvent.click(button);
-   expect(onClick).toHaveBeenCalledTimes(1);
-})
-*/;
+
+// test('Right onClick is called', () => {
+//    const onClick = jest.fn();
+//    const itemCard = render(<ItemCard image = {null} name = {null} price = {null} link = {null} sale = {null} onClick = {onClick}/>);
+//    const button  = itemCard.getByRole('ItemCard');
+//    fireEvent.click(button);
+//    expect(onClick).toHaveBeenCalledTimes(1);
+// })
